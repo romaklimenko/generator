8537dd25c71575c88b8b2c6a3eebc65708104321
romaklimenko <romaklimenko@gmail.com>
2013-08-12 16:34:20 +0300

    Initial service.aspx
    /sitecore modules/Shell/Sitecore.Analytics.DataGenerator/service.aspx

---
+++ Website/sitecore modules/Shell/Sitecore.Analytics.DataGenerator/Service.aspx.cs
@@ -0,0 +1,17 @@
+﻿namespace Sitecore.Analytics.DataGenerator
+{
+  using System;
+  using System.Web.UI;
+
+  public partial class Service : Page
+  {
+    protected void Page_Load(object sender, EventArgs e)
+    {
+      var json = "{\"namespace\":\"Sitecore.Analytics.DataGenerator\"}";
+      this.Response.Clear();
+      this.Response.ContentType = "application/json; charset=utf-8";
+      this.Response.Write(json);
+      this.Response.End();
+    }
+  }
+}
